61635664d9ccaacdc97aea2a0291ce45a98a57e3
vinilive <pastorvini99@gmail.com>
2025-02-24 23:59:00 +0000

    em andamento

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,6 +2,18 @@
 
 int  main(){
      printf("desafio super trunfo!\n");
+     printf("novo comit\n");
+     int carta = 1;
+     char estado = 'a';
+     int codigo= 'a01';
+     char cidade[]= "sao paulo";
+     long long população = 125000;
+     double area= 2500.45;
+     double pib= 699.28 ;
+     int ponto_turisticos= 27;
+
+     
+
 
 
 
