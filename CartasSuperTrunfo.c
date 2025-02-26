9013102e525afc37f994f54f6d9e64cc41feb7ec
vinilive <pastorvini99@gmail.com>
2025-02-26 08:26:35 +0000

    carta a terminada

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,29 +1,54 @@
 #include <stdio.h>
 
-int  main(){
-     printf("desafio super trunfo!\n");
-     printf("novo comit\n");
-     int carta = 1;
-     char estado = 'a';
-     int codigo= 'a01';
-     char cidade[]= "sao paulo";
-     long long população = 125000;
-     double area= 2500.45;
-     double pib= 699.28 ;
-     int ponto_turisticos= 27;
+int main() {
+    printf("Desafio Super Trunfo!\n");
+    printf("Novo commit\n");
 
-     printf("digite a carta:")
-     scanf("%d", &carta);
-     printf("a carta é: %d\n",carta);
+    char estado[20]= "ceara";
+    char codigo[10]= "a01";
+    char cidade[20] = "fortaleza";
+    int populacao = 125000;  
+    float area_em_km2 = 2500.45;  
+    float pib = 699.28;
+    int pontos_turisticos = 27;
 
-     printf("digite o estado:");
-     scanf("%s" , &estado);
-     printf("o estado é: %S\n", estado);
+    printf("Digite o estado: ");
+    scanf("%s", estado);  
+    printf("O estado é: %s\n", estado);
 
-     printf(" digite o codigo:");
-     scanf("%d" &codigo);
-     printf("o codigo é: %d\n" ,codigo);
+    printf("Digite o código: ");
+    scanf("%s", codigo);
+    printf("O código é: %s\n", codigo);
+
+    printf("Digite a cidade: ");
+    scanf("%s", cidade);
+    printf("A cidade é: %s\n", cidade);
+
+    printf("Digite a população: ");
+    scanf("%d", &populacao);  
+    printf("A população é: %d\n", populacao);
+
+    printf("digite a area em km²: ");
+    scanf("%f", &area_em_km2);
+    printf("a area em km² é: %f\n", area_em_km2);
+
+    printf("digite o pib: ");
+    scanf("%f", &pib) ;
+    printf("o pib é: %f\n", pib);
+
+    printf("digite os pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("os pontos turisticos sao: %d\n", pontos_turisticos);
+
+    return 0;
 
 
 
-}
+    }
+
+
+
+
+
+
+
