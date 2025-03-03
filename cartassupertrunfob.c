f92106e3929e808ee0e29303a08fa382c147b1f7
vinilive <pastorvini99@gmail.com>
2025-03-03 23:58:48 +0000

    estrutura de decisao

--- cartassupertrunfob.c
+++ cartassupertrunfob.c
@@ -4,44 +4,31 @@ int main() {
     printf("Desafio Super Trunfo!\n");
     printf("Novo commit\n");
 
-    char estado[20]= "PR";
-    char codigo[10]= "a02";
-    char cidade[20] = "curitiba";
-    int populacao = 128000;  
-    float area_em_km2 = 2900.45;  
-    float pib = 619.48;
-    int pontos_turisticos = 17;
+    char estado1[20]= "SP";
+    char estado2[20]= "RJ";
+    char codigo1[10]= "a01";
+    char codigo2[10]= "a02";
+    char cidade1[20] = "sao paulo";
+    char cidade2[20] = "sao paulo";
+    int populacao1 = 125000;  
+    int populacao2 = 123000;  
+    float area_em_km2_1 = 2500.45;
+    float area_em_km2_2 = 2300.45;   
+    float pib1 = 699.28;
+    float pib2 = 690.28;
+    int pontos_turisticos1 = 27;
+    int pontos_turisticos2 = 20;
+    
+    if (pib1 > pib2){
 
-    printf("Digite o estado: ");
-    scanf("%s", estado);  
-    printf("O estado é: %s\n", estado);
+        printf("a carta 1 é mais forte que a carta 2 \n");
+    }else {
 
-    printf("Digite o código: ");
-    scanf("%s", codigo);
-    printf("O código é: %s\n", codigo);
-
-    printf("Digite a cidade: ");
-    scanf("%s", cidade);
-    printf("A cidade é: %s\n", cidade);
-
-    printf("Digite a população: ");
-    scanf("%d", &populacao);  
-    printf("A população é: %d\n", populacao);
-
-    printf("digite a area em km²: ");
-    scanf("%f", &area_em_km2);
-    printf("a area em km² é: %f\n", area_em_km2);
-
-    printf("digite o pib: ");
-    scanf("%f", &pib) ;
-    printf("o pib é: %f\n", pib);
-
-    printf("digite os pontos turisticos: ");
-    scanf("%d", &pontos_turisticos);
-    printf("os pontos turisticos sao: %d\n", pontos_turisticos);
-
-    return 0;
+        printf(" a carta 2 é mais forte que a carta 1\n");    
+    } 
+     return 0;
 
 
 
-    }
+
+}
